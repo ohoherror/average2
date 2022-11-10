035523d1f45b95fab27a20f62e9b87bf022615eb
ohoherror <1458251589@qq.com>
2022-11-10 10:58:58 +0800

    count average

--- main.go
+++ main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
 	fmt.Println(os.Args[1:])
+	arguments := os.Args[1:]
+	var sum float64 = 0
+	for _, argument := range arguments {
+		number, err := strconv.ParseFloat(argument, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sum += number
+	}
+	sampleCount := float64(len(arguments))
+	fmt.Println("Average:%0.2f\n", sum/sampleCount)
 }
